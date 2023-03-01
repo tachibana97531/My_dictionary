98db2bb825b65d93d205c203bf469bbe253ee803
tachibana97531 <122186925+tachibana97531@users.noreply.github.com>
2023-03-01 00:42:53 +0000

    [add]辞書の編集、削除機能

--- app/controllers/public/dictionaries_controller.rb
+++ app/controllers/public/dictionaries_controller.rb
@@ -22,22 +22,41 @@ class Public::DictionariesController < ApplicationController
   end
 
   def show
+    @dictionary = Dictionary.find(params[:id])
+    @tag_list = @dictionary.tags.pluck(:tag_name).join(',')
   end
 
   def edit
+    @dictionary = Dictionary.find(params[:id])
+    @tag_list=@dictionary.tags.pluck(:tag_name).join(',')
   end
 
   def update
+    @dictionary = Dictionary.find(params[:id])
+    tag_list = params[:dictionary][:tag_name].split(',')
+    if @dictionary.update(dictionary_params)
+      @old_relations = TagPost.where(dictionary_id:@dictionary.id)
+      @old_relations.each do |relations|
+        relations.delete
+      end
+      @dictionary.save_tag(tag_list)
+      redirect_to dictionary_path(@dictionary.id)
+    else
+      render edit
+    end
   end
 
   def destroy
+    @dictionary = Dictionary.find(params[:id])
+    @dictionary.destroy
+    redirect_to dictionaries_path
   end
 
 
   private
 
   def dictionary_params
-    params.require(:dictionary).permit(:title,:summery,:post_status,:dictionary_id)
+    params.require(:dictionary).permit(:image,:title,:summery,:post_status,:dictionary_id)
   end
 
 end
--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -9,12 +9,12 @@ class Dictionary < ApplicationRecord
 
   enum post_status: {closing:0,opening:1}
 
-  def get_image(width,height)
-    unless image.attached?
-      file_path = Rails.root.join("no_image.jpg")
-      image.attach(io:File.open(file_path),file_name:"default-image.jpg",content_type:"image/jpeg")
-    end
-    image.variant(resize_to_limit:[width,height]).processed
+  def get_image(width, height)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+    image.variant(resize_to_limit: [width, height])
   end
 
   def save_tag(sent_tags)
