8380d94415d097145135f2737f3268e83a08294c
tachibana97531 <122186925+tachibana97531@users.noreply.github.com>
2023-03-04 09:30:01 +0000

    [add]単語の編集、削除機能

--- app/controllers/public/details_controller.rb
+++ app/controllers/public/details_controller.rb
@@ -23,12 +23,24 @@ class Public::DetailsController < ApplicationController
   end
 
   def edit
+    @dictionary = Dictionary.find(params[:dictionary_id])
+    @detail = Detail.find(params[:id])
   end
 
   def update
+    @dictionary = Dictionary.find(params[:dictionary_id])
+    @detail = Detail.find(params[:id])
+    @detail.dictionary_id = @dictionary.id
+    @detail.update(detail_params)
+    redirect_to dictionary_detail_path(@dictionary.id,@detail.id)
   end
 
   def destroy
+    @dictionary = Dictionary.find(params[:dictionary_id])
+    @detail = Detail.find(params[:id])
+    @detail.dictionary_id = @dictionary.id
+    @detail.destroy
+    redirect_to dictionary_details_path(@dictionary.id)
   end
 
 
