ad4fbeb6c32d65256e740500a61646bf7c8d6451
tachibana97531 <122186925+tachibana97531@users.noreply.github.com>
2023-03-04 12:14:34 +0000

    [add]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
+   devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+   end
+
    devise_for :admin,skip:[:registrations,:passwords],controllers: {
     sessions: "admin/sessions"
   }
 
   devise_for :users, controllers: {
     registrations: "public/registrations",
-    sessions: "public/sessions"
+    sessions: "public/sessions",
+    passwords: 'users/passwords'
   }
 
   get "search" => "searches#search"
